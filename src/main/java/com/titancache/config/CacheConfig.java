d485c53b9a46036cc25e4c9137a84fb77bf9242d
imrahn <35425886+ImrahnF@users.noreply.github.com>
2025-12-13 16:27:41 -0500

    Created config

---
+++ src/main/java/com/titancache/config/CacheConfig.java
@@ -0,0 +1,14 @@
+package com.titancache.config;
+
+import com.titancache.core.TitanCache;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public TitanCache<String, String> titanCache() {
+        return new TitanCache<>(100);
+    }
+}
